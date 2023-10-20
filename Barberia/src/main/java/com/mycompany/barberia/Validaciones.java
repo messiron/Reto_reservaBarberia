c3a81d523ccaed4ab670bdf447c85c5c3de6838c
sgb06 <santigallego2706@gmail.com>
2023-10-20 18:24:15 -0500

    Validaciones

---
+++ Barberia/src/main/java/com/mycompany/barberia/Validaciones.java
@@ -0,0 +1,38 @@
+
+package com.mycompany.barberia;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Validaciones {
+
+    public Validaciones() {
+    }
+   
+    public boolean validarExistencia (int id){
+        Cliente cliente = new Cliente();
+        HashMap <Integer, Integer> mapaClientes = new HashMap<>();
+        try{
+        FileInputStream archivo = new FileInputStream("Clientes.txt");
+            ObjectInputStream objectInputStream = new ObjectInputStream(archivo);
+            while(true){
+             try{
+            cliente=(Cliente)objectInputStream.readObject(); 
+            mapaClientes.put(cliente.getId(), cliente.getId());
+                 System.out.println(cliente.getId());
+             }catch(Exception e){
+                 break;
+             }
+            }
+            archivo.close();
+            objectInputStream.close();        
+        
+               
+    }catch(Exception e){
+    }
+         boolean resulatado=mapaClientes.containsValue(id);
+         System.out.println(resulatado);
+        return resulatado;
+}
+}
